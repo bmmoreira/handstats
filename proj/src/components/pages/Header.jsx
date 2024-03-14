786bf8f9a991dcc654a9d99b8870ade4dd92d0e1
bmmoreira <bruno.medeiros.moreira@gmail.com>
2024-03-14 10:21:27 +0000

    Home updates

--- proj/src/components/pages/Header.jsx
+++ proj/src/components/pages/Header.jsx
@@ -1,4 +1,4 @@
-import React, { useContext } from 'react';
+/* import React, { useContext } from 'react';
 import StateContext from '../../StateContext';
 
 function Header(props) {
@@ -6,6 +6,79 @@ function Header(props) {
 
   return (
     <header className="">{appState.loggedIn ? "Logado" : "Não Logado"}</header>
+  );
+}
+
+export default Header; */
+
+import React, { useContext } from "react";
+import DispatchContext from "../../DispatchContext";
+import StateContext from "../../StateContext";
+import { useLocation } from "react-router-dom";
+import HelpIcon from "@mui/icons-material/Help";
+import AppBar from "@mui/material/AppBar";
+import Toolbar from "@mui/material/Toolbar";
+import Box from "@mui/material/Box";
+import IconButton from "@mui/material/IconButton";
+import Typography from "@mui/material/Typography";
+import MenuIcon from "@mui/icons-material/Menu";
+import { roundedButton } from "../Utils/constants.js";
+
+function Header() {
+  const appDispatch = useContext(DispatchContext);
+  const appState = useContext(StateContext);
+  const location = useLocation();
+
+  function showHelpDialog() {
+    appDispatch({
+      type: "toggleHelpDialog",
+      value: true,
+    });
+    console.log("HELP CLICKED!");
+  }
+
+  return (
+    <Box sx={{ flexGrow: 1 }}>
+      <AppBar position="static">
+        <Toolbar>
+          <IconButton
+            onClick={() => {
+              console.log("Menu Clicked!");
+            }}
+            size="large"
+            edge="start"
+            color="inherit"
+            aria-label="open drawer"
+            sx={{ mr: 2 }}
+          >
+            <MenuIcon />
+          </IconButton>
+          <Typography
+            variant="h6"
+            noWrap
+            component="div"
+            sx={{ flexGrow: 1, display: { xs: "none", sm: "block" } }}
+          >
+            HandyStats
+          </Typography>
+          {(location.pathname === "/") |
+          (location.pathname === "/statistics") |
+          (location.pathname === "/options") ? (
+            ""
+          ) : (
+            <>
+              <IconButton
+                aria-label="help"
+                sx={{ ...roundedButton, marginLeft: "5px" }}
+                onClick={showHelpDialog}
+              >
+                <HelpIcon />
+              </IconButton>
+            </>
+          )}
+        </Toolbar>
+      </AppBar>
+    </Box>
   );
 }
 
