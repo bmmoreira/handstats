3840e84a8a3569fe091c4405eb69567e89f7ba42
bmmoreira <bruno.medeiros.moreira@gmail.com>
2024-03-18 19:18:37 +0000

    Timeline updates

--- proj/src/components/Handball/GameTimeLine.jsx
+++ proj/src/components/Handball/GameTimeLine.jsx
@@ -1,4 +1,6 @@
-import * as React from 'react';
+import React, { useContext } from "react";
+import DispatchContext from "../../DispatchContext";
+import StateContext from "../../StateContext";
 import Timeline from '@mui/lab/Timeline';
 import TimelineItem from '@mui/lab/TimelineItem';
 import TimelineSeparator from '@mui/lab/TimelineSeparator';
@@ -19,63 +21,68 @@ import "./gametimeline.css";
 import { minWidth } from '@mui/system';
 
 export default function GameTimeline() {
+  const appDispatch = useContext(DispatchContext);
+  const appState = useContext(StateContext);
+
+  const formatTime = (timeInSeconds) => {
+    const hours = Math.floor(timeInSeconds / 3600);
+    const minutes = Math.floor((timeInSeconds % 3600) / 60);
+    const seconds = timeInSeconds % 60;
+  
+    return `${padZero(minutes)}:${padZero(seconds)}`;
+  };
+
+  const padZero = (num) => {
+    return num.toString().padStart(2, '0');
+  };
+
+  const selectActionIcon = (type) => {
+    switch (type) {
+      case "attack":
+        return <SportsHandballIcon/>;
+      case "defense":
+        return <SportsKabaddiIcon/>;
+      case "sanction":
+        return <SimCardAlertIcon/>;
+    }
+  };
+
   return (
     <Timeline position="right">
-        <TimelineItem sx={{ minWidth: '350px'}}>
+
+    {appState.gameActions.map((item) => (
+             <TimelineItem sx={{ minWidth: '350px'}}>
 
           
-        <TimelineOppositeContent
-          sx={{ m: 'auto 0' }}
-          align="right"
-          variant="body2"
-          color="text.secondary"
-        >
-          00:35 min Half 1
-        </TimelineOppositeContent>
-        <TimelineSeparator>
-          <TimelineConnector />
-          <TimelineDot>
-            <ArrowUpwardIcon />
-          </TimelineDot>
-          <TimelineConnector />
-        </TimelineSeparator>
-        <TimelineContent  sx={{ m: 'auto 0' }}
-          align="right"
-          variant="body2"
-          color="text.secondary">
-          
-            Player in 12
+             <TimelineOppositeContent
+               sx={{ m: 'auto 0' }}
+               align="right"
+               variant="body2"
+               color="text.secondary"
+             >
+               {formatTime(item.time)} min Half 1
+             </TimelineOppositeContent>
+             <TimelineSeparator>
+               <TimelineConnector />
+               <TimelineDot>
+                {selectActionIcon(item.action)}
+               </TimelineDot>
+               <TimelineConnector />
+             </TimelineSeparator>
+             <TimelineContent  sx={{ m: 'auto 0' }}
+               align="right"
+               variant="body2"
+               color="text.secondary">
+               
+                 Player {item.player} - {item.actionName} 
+     
+             </TimelineContent>
+           </TimelineItem>
+            ))}
 
-        </TimelineContent>
-      </TimelineItem>
+        
 
-      <TimelineItem sx={{ minWidth: '350px'}}>
-        <TimelineOppositeContent
-          sx={{ m: 'auto 0' }}
-          align="right"
-          variant="body2"
-          color="text.secondary"
-        >
-          01:17 min Half 1
-        </TimelineOppositeContent>
-        <TimelineSeparator>
-          <TimelineConnector />
-          <TimelineDot>
-            <SimCardAlertIcon />
-          </TimelineDot>
-          <TimelineConnector />
-        </TimelineSeparator>
-        <TimelineContent  sx={{ m: 'auto 0' }}
-          align="right"
-          variant="body2"
-          color="text.secondary">
-          
-            Yellow Card 7
-
-        </TimelineContent>
-      </TimelineItem>
-
-      
+           
     </Timeline>
   );
 }
